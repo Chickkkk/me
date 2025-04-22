97f333f71a9b06b68dde36cefb1f25e37d288ed0
qingkun <632732162@qq.com>
2025-04-22 17:54:30 +0800

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Index</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import baobao from '@/assets/imgs/baobao.png'
+import dog from '@/assets/imgs/dog.png'
+
+</script>
+
+<style lang="scss" scoped>
+</style>
