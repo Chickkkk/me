40636c69b30a49783b4bfe829c1601b2de26a35c
Chickkkk <64410550+Chickkkk@users.noreply.github.com>
2025-05-08 14:07:40 +0000

    Deploying to gh-pages from @ Chickkkk/me@5a07d70fd0ea86b8567007769106824474093d1c 🚀

--- app/composables/heart.ts
+++
@@ -1,1 +0,0 @@
-export const useHeart = () => useState('heart', () => 0)
--- app/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-  <UContainer class="max-w-2xl min-h-screen">
-    <slot />
-  </UContainer>
-</template>
